476a84e6b4cbce28169f1a011149b832083eb0e7
Aaron Ogle <atogle@gmail.com>
2014-01-17 11:40:56 -0500

    Update for Django 1.6

--- django-csrf.js
+++ django-csrf.js
@@ -15,50 +15,44 @@ service authenticated as the owner of this dataset, we want to protect the API
 view against CSRF. In order to ensure that AJAX POST/PUT/DELETE requests that
 are made via jQuery will not be caught by the CSRF protection, we use the
 following code. For more information, see:
-https://docs.djangoproject.com/en/1.4/ref/contrib/csrf/
-
+https://docs.djangoproject.com/en/1.6/ref/contrib/csrf/
 */
 
-jQuery(document).ajaxSend(function(event, xhr, settings) {
-    function getCookie(name) {
-        var cookieValue = null;
-        if (document.cookie && document.cookie !== '') {
-            var cookies = document.cookie.split(';');
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = jQuery.trim(cookies[i]);
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
+jQuery(function() {
+  function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+        var cookie = jQuery.trim(cookies[i]);
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) === (name + '=')) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
         }
-        return cookieValue;
+      }
     }
-    function sameOrigin(url) {
-        // url could be relative or scheme relative or absolute
-        var host = document.location.host; // host + port
-        var protocol = document.location.protocol;
-        var sr_origin = '//' + host;
-        var origin = protocol + sr_origin;
-        // Allow absolute or scheme relative URLs to same origin
-        return (url == origin || url.slice(0, origin.length + 1) == origin + '/') ||
-            (url == sr_origin || url.slice(0, sr_origin.length + 1) == sr_origin + '/') ||
-            // or any other URL that isn't scheme relative or absolute i.e relative.
-            !(/^(\/\/|http:|https:).*/.test(url));
-    }
-    function safeMethod(method) {
-        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-    }
+    return cookieValue;
+  }
 
-    if (!safeMethod(settings.type) && sameOrigin(settings.url)) {
+  function safeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+  }
+
+  jQuery.ajaxSetup({
+    crossDomain: false, // obviates need for sameOrigin test
+    beforeSend: function(xhr, settings) {
+      if (!safeMethod(settings.type)) {
         xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-    }
+      }
 
-    // If this is a DELETE request, explicitly set the data to be sent so that
-    // the browser will calculate a value for the Content-Length header.
-    if (settings.type === 'DELETE') {
+      // If this is a DELETE request, explicitly set the data to be sent so that
+      // the browser will calculate a value for the Content-Length header.
+      if (settings.type === 'DELETE') {
         xhr.setRequestHeader("Content-Type", "application/json");
         settings.data = '{}';
+      }
     }
+  });
 });
